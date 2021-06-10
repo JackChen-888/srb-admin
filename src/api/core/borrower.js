dcec8ff556231888180b5cf7488e264703c69abc
liuchen <liuchen525@126.com>
2021-06-10 16:09:19 +0800

    借款人信息展示

--- src/api/core/borrower.js
+++ src/api/core/borrower.js
@@ -7,5 +7,12 @@ export default {
       method: 'get',
       params: {keyword}
     })
+  },
+
+  show(id) {
+    return request({
+      url: `/admin/core/borrower/show/${id}`,
+      method: 'get'
+    })
   }
 }
